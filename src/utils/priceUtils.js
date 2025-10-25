7d7b8762168176f1b667c39a8fb6ab1b7cae45bf
alaa8ali <alaa4mange1@gmail.com>
2025-10-25 19:54:04 +0300

    Update priceUtils.js

--- src/utils/priceUtils.js
+++ src/utils/priceUtils.js
@@ -1,1 +1,12 @@
+export function calculateProfit(cost, price) {
+  return (price - cost).toFixed(2);
+}
+
+export function formatCurrency(value) {
+  return `${parseFloat(value).toFixed(2)} د.ع`;
+}
+
+export function applyDiscount(price, discount) {
+  return (price * (1 - discount / 100)).toFixed(2);
+}
 
